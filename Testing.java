78cdccd684ae5734b34c49db95957e99572d9e23
Dhinesh system <dhinesh.ganapathy@ideas2it.com>
2021-05-19 19:42:09 +0530

    Master slave with git hook

--- Testing.java
+++ Testing.java
@@ -1,6 +1,6 @@
 public class Testing {
   public static void main(String args[]) {
-    System.out.println("Web hook done");
+    System.out.println("Web hook done along with master slave");
   }
 
 }
